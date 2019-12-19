35e36434cac2e4cd79e78ec13d65a8e27e5c432f
Season000 <43396528+Season000@users.noreply.github.com>
2019-12-19 14:42:18 +0800

    12.19

---
+++ zidian.py
@@ -0,0 +1,11 @@
+# 字典
+brand = ['李宁','耐克','阿迪达西','season']
+slogan = ['everthing is imposible','just do it','impossible is nothing','do it']
+print("李宁的口号是：",slogan[brand.index('耐克')])
+
+
+dict1 = {'李宁':'everthing is imposible',
+         '耐克':'just do it',
+         '阿迪达西':'impossible is nothing',
+         'season':'do it'}
+print("season 的口号是：",dict1['season'])
